1bdd859df26672cd6db3b5f86263daec124e0017
Quad400 <adedijiabdulquadri@gmail.com>
2023-05-24 23:31:37 +0100

    protect environment variable

--- app/config/migrate.sh
+++ app/config/migrate.sh
@@ -2,7 +2,11 @@
 
 # navigate to the docker-created 
 # django project root directory
+DJANGO_SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-"acer@gmail.com"}
 cd /app/
 
+
 # migrate a docker container as needed
-/opt/venv/bin/python manage.py migrate
+/opt/venv/bin/python manage.py migrate --noinput
+# /opt/venv/bin/python manage.py collectstatic
+/opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --noinput || true
